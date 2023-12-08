e2f52b15d3d3fb67aff6c0d44edd9c8ad14725ed
FrederickxG <98033489+FrederickxG@users.noreply.github.com>
2023-12-07 23:20:23 -0500

    Revert "Entrant 0.8 (clipboard pick up and boss*beta*)"
    This reverts commit 7232a7c989bfbcc48132621a86cd312a5aaf3fe0.

--- Assets/Scripts/InteractTest.cs
+++ Assets/Scripts/InteractTest.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class InteractTest : MonoBehaviour, IInteractable
 {
-    // used to test interaction functions
-
-     public GameObject collect;
-     public GameObject Boss;
+    // used to test interaction
    public void Interact() 
    {
-    Debug.Log("working");
-     if (gameObject == collect)
-        {
-            // Collect 
-            collect.SetActive(false);
-
-            Boss.SetActive(true);
-        }
+    Debug.Log("Test");
    }
 
 }
