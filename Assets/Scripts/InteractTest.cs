fc0a40a9e40f04f64ddf88a256a9d6e552f4cd66
FrederickxG <98033489+FrederickxG@users.noreply.github.com>
2023-12-14 13:53:51 -0500

    Place enemy back and attempting ring bar

--- Assets/Scripts/InteractTest.cs
+++ Assets/Scripts/InteractTest.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class InteractTest : MonoBehaviour, IInteractable
 {
+   public GameObject collect;
+     public GameObject Boss;
     // used to test interaction
    public void Interact() 
    {
+    Debug.Log("working");
+     if (gameObject == collect)
+        {
+            // Collect 
+            collect.SetActive(false);
+
+            Boss.SetActive(true);
+        }
     Debug.Log("Test");
    }
-
 }
