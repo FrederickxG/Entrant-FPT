e07aba69656cc0ed9de0ce535c217a20c6051ff2
FrederickxG <98033489+FrederickxG@users.noreply.github.com>
2024-01-15 22:35:02 -0500

    Entrant V1 pt2.1

---
+++ Assets/Scripts/BGMAudio.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+    }
+}
+
