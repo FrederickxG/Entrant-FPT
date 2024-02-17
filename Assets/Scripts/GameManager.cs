a1b489898f6e3d74b77ad1a313e991ff5b0c9753
FrederickxG <98033489+FrederickxG@users.noreply.github.com>
2024-02-16 20:47:49 -0500

    Entrant V1.5  (WIP)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+     public GameObject freya; 
+     public AudioSource audioSource;
+
+    // Start is called before the first frame update
+ void Start()
+{
+    freya.SetActive(true);
+    audioSource.Play();
+}
+
+// Update is called once per frame
+void Update()
+{
+    if (!audioSource.isPlaying)
+    {
+        freya.SetActive(false);
+    }
+}
+}
